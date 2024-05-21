ba3ab88743cf2b6acac90d24102e398d9713db50
realcold0 <realcold0@gmail.com>
2024-05-21 16:48:13 +0900

    feat : 버튼 누를시 obj들이 내려오고 화면 전환

--- wadada/main.js
+++ wadada/main.js
@@ -18,62 +18,68 @@ import Matter from 'matter-js'
  */
 
 // module aliases
-
-const browserWidth = document.querySelector("#container").clientWidth;
-const browserHeight = document.querySelector("#container").clientHeight;
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite,
-    Composties = Matter.Composites;
-
-
-// create an engine
-var engine = Engine.create();
-
-// create a renderer
-var render = Render.create({
-    element: document.getElementById("container"),
-    engine: engine,
-    options:{
-      width: browserWidth,
-      height : browserHeight,
-      wireframes: false,
-    }
-});
-
-// create two boxes and a ground
-let startBtn = Bodies.rectangle(browserHeight)
-var ground = Bodies.rectangle(browserWidth/2, browserHeight, browserWidth, 40, { isStatic: true });
-var wallLeft = Bodies.rectangle(0, browserHeight, 10, browserHeight, { isStatic: true });
-var wallRight = Bodies.rectangle(browserWidth, browserHeight, 10, browserHeight, { isStatic: true });
-
-let stack1 = Composties.stack(200, 200, 10, 2, 50,  50, function(x, y) {
-  return Bodies.rectangle(x, y, 40, 40);
-});
-let stack2 = Composties.stack(200, 200, 14, 1, 100,  800, function(x, y) {
-  return Bodies.rectangle(x, y, 40, 40);
-});
-// add all of the bodies to the world
-Composite.add(engine.world, [
-  //wall
-  ground,
-  wallLeft,
-  wallRight
-]);
-
-Composite.add(engine.world, [stack1, stack2])
-
-// run the renderer
-Render.run(render);
-
-// create runner
-var runner = Runner.create();
-
-// run the engine
-Runner.run(runner, engine);
+const startLoading = function(){
+  const browserWidth = document.querySelector("#container").clientWidth;
+  const browserHeight = document.querySelector("#container").clientHeight;
+  var Engine = Matter.Engine,
+      Render = Matter.Render,
+      Runner = Matter.Runner,
+      Bodies = Matter.Bodies,
+      Composite = Matter.Composite,
+      Composties = Matter.Composites;
 
 
+  // create an engine
+  var engine = Engine.create();
+
+  // create a renderer
+  var render = Render.create({
+      element: document.getElementById("container"),
+      engine: engine,
+      options:{
+        width: browserWidth,
+        height : browserHeight,
+        wireframes: false,
+      }
+  });
+
+  // create two boxes and a ground
+  let startBtn = Bodies.rectangle(browserHeight)
+  var ground = Bodies.rectangle(browserWidth/2, browserHeight, browserWidth, 40, { isStatic: true });
+  var wallLeft = Bodies.rectangle(0, browserHeight /2, 10, browserHeight, { isStatic: true });
+  var wallRight = Bodies.rectangle(browserWidth, browserHeight/2, 10, browserHeight, { isStatic: true });
+
+  let stack1 = Composties.stack(0, -500, 10, 6, 5,  5, function(x, y) {
+    return Bodies.rectangle(x, y, 40, 40);
+  });
+  // add all of the bodies to the world
+  Composite.add(engine.world, [
+    //wall
+    ground,
+    wallLeft,
+    wallRight
+  ]);
+
+  Composite.add(engine.world, [stack1])
+
+  // run the renderer
+  Render.run(render);
+
+  // create runner
+  var runner = Runner.create();
+
+  // run the engine
+  Runner.run(runner, engine);
+}
+
+let startBtn = document.getElementById("startBtn");
+startBtn.addEventListener("click", ()=>{
+  startBtn.style.display = "none"
+  startLoading();
+
+  setTimeout(() =>{
+    window.location.href = 'app.html'
+  } , 3000);
+})
 
 setupCounter(document.querySelector('#counter'))
