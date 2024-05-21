e270db754d9e690a31cf2b296dc6aed3198f859d
lee-junr <wns2610@icloud.com>
2024-05-21 15:02:30 +0900

    feat : 이미지 생성 및 추가 기능 구현

--- wadada/game.js
+++ wadada/game.js
@@ -1,4 +1,5 @@
-import { Bodies, Engine, Render, Runner, World } from "matter-js";
+import { Bodies, Engine, Render, Runner, World, Body } from "matter-js";
+import { IMAGES } from "./image";
 
 const engine = Engine.create();
 const render = Render.create({
@@ -32,10 +33,58 @@ const ground = Bodies.rectangle(310, 820, 620, 60, { // x,y,height,width
 /** 천장 벽 */
 const topLine = Bodies.rectangle(310, 150, 620, 2, { // x,y,height,width
   isStatic: true,
+  isSensor: true,
   render: {fillStyle: "#000000"}
 });
 
-World.add(world, [leftWall, rightWall, ground]);
+World.add(world, [leftWall, rightWall, ground, topLine]);
 
 Render.run(render);
-Render.run(engine);
+Runner.run(engine);
+
+let currentBody = null;
+let currentImage = null;
+
+/** 과일을 생성하는 함수 */
+function addImage(){
+  const index = Math.floor(Math.random() * 3);
+  const image = IMAGES[index];
+
+  const body = Bodies.circle(300, 50, image.radius,{
+    isSleeping: true, // 이벤트 전까지 더이상 떨어지지 않고 keep 한 상태가 됨.
+    render:{
+      sprite: { texture: `${image.label}.png` }
+    },
+    restitution: 0.2 // 과일의 탄성 설정
+  });
+
+
+  currentBody = body;
+  currentImage = image;
+
+  World.add(world, body);
+}
+/** 키보드 입력 구현
+ * onkeydown : 자바스크립트의 기본적인 키보드 이벤트 함수 */
+
+window.onkeydown = (event) => {
+  switch (event.code) {
+    case "KeyA": 
+      Body.setPosition(currentBody,{
+        x: currentBody.position.x - 10, // A 키를 두를때마다 왼쪽으로 10만큼 이동
+        y: currentBody.position.y,
+      });
+      break;
+    case "KeyD": 
+      Body.setPosition(currentBody,{
+        x: currentBody.position.x + 10, // A 키를 두를때마다 왼쪽으로 10만큼 이동
+        y: currentBody.position.y,
+      });
+      break;
+    case "KeyS": 
+      currentBody.isSleeping = false;
+      addImage();
+      break;
+  }
+}
+addImage();
---
+++ wadada/image.js
@@ -0,0 +1,15 @@
+export const IMAGES = [
+  {
+    label: "00_cherry",
+    radius: 120 / 2,
+  },
+  {
+    label: "01_strawberry",
+    radius: 120 / 2,
+  },
+  {
+    label: "02_grape",
+    radius: 61 / 2,
+  },
+  
+];
